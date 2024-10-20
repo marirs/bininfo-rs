5fd5a4f014b4f05ccf3b7a71c29d0178f4317c2c
Andrey Mnatsakanov <andrey@3ig.dev>
2024-10-20 12:06:42 +0200

    make TLS directory optional

--- src/pe/tls.rs
+++ src/pe/tls.rs
@@ -10,18 +10,22 @@ pub struct TlsCallbacks {
 impl TlsCallbacks {
     pub fn parse(pe: &VecPE) -> Result<TlsCallbacks> {
         let callbacks = match pe.get_arch()? {
-            exe::Arch::X86 => {
-                let tls = ImageTLSDirectory32::parse(pe)?;
-                tls.get_callbacks(pe)?
+            exe::Arch::X86 => match ImageTLSDirectory32::parse(pe) {
+                Ok(tls) => tls
+                    .get_callbacks(pe)?
                     .iter()
                     .map(|x| x.0.into())
-                    .collect::<Vec<u64>>()
-            }
-            exe::Arch::X64 => ImageTLSDirectory64::parse(pe)?
-                .get_callbacks(pe)?
-                .iter()
-                .map(|x| x.0)
-                .collect::<Vec<u64>>(),
+                    .collect::<Vec<u64>>(),
+                _ => vec![],
+            },
+            exe::Arch::X64 => match ImageTLSDirectory64::parse(pe) {
+                Ok(tls) => tls
+                    .get_callbacks(pe)?
+                    .iter()
+                    .map(|x| x.0)
+                    .collect::<Vec<u64>>(),
+                _ => vec![],
+            },
         };
         Ok(TlsCallbacks { callbacks })
     }
